5191fd702f790f16104046995493c95f65bf7781
squidman <squidman@outlook.com>
2021-02-11 08:51:24 -0800

    Saving progress.

--- MM2RandoLib/Extensions/StringExtensions.cs
+++ MM2RandoLib/Extensions/StringExtensions.cs
@@ -45,6 +45,30 @@ namespace MM2Randomizer.Extensions
             return returnValue;
         }
 
+
+        public static Int32 ToHash32(this String in_String)
+        {
+            // Convert the String to an array for hashing
+            Byte[] seedStringBytes = Encoding.ASCII.GetBytes(in_String);
+
+            // Create a new config in order to hash to a 32-bit number
+            FNVConfig c = new FNVConfig()
+            {
+                HashSizeInBits = 32,
+                Prime = new System.Numerics.BigInteger(1099511628211),
+                Offset = new System.Numerics.BigInteger(14695981039346656037),
+            };
+
+            // Compute the hash
+            IHashValue hashValue = FNV1aFactory.Instance.Create(c).ComputeHash(seedStringBytes);
+
+            // Copy the hash to the Int32 seed
+            Int32[] array = new Int32[1];
+            hashValue.AsBitArray().CopyTo(array, 0);
+            return array[0];
+        }
+
+
         public static Byte[] AsIntroString(this String in_String)
         {
             if (null == in_String)
---
+++ MM2RandoLib/Utilities/Seed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MM2RandoLib.Utilities
+{
+    /// <summary>
+    /// Provides basic randomization methods
+    /// </summary>
+    public sealed class Seed
+    {
+        //
+        // Constructors
+        //
+
+        //
+        // Public Static Methods
+        //
+
+        public static Seed FromString(String in_SeedString)
+        {
+        }
+
+
+        //
+        // Private Data Members
+        //
+
+        private Random mRandom;
+    }
+}
