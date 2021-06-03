896d1c695b87f57de668b6a83c5615ea53456f09
squidman <squidman@outlook.com>
2021-06-03 12:31:46 -0700

    Updating to the latest Avalonia release.

---
+++ RandomizerHost/ViewLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using RandomizerHost.ViewModels;
+
+namespace RandomizerHost
+{
+    public class ViewLocator : IDataTemplate
+    {
+        public bool SupportsRecycling => false;
+
+        public IControl Build(object data)
+        {
+            String name = data.GetType().FullName!.Replace("ViewModel", "View");
+            Type type = Type.GetType(name);
+
+            if (type != null)
+            {
+                return (Control)Activator.CreateInstance(type)!;
+            }
+            else
+            {
+                return new TextBlock { Text = "Not Found: " + name };
+            }
+        }
+
+        public bool Match(object data)
+        {
+            return data is ViewModelBase;
+        }
+    }
+}
+
