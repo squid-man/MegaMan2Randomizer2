2c728e88f619f5352938e8c155c96ed70de41692
squidman <squidman@outlook.com>
2021-06-05 14:07:26 -0700

    Updating avalonia to 0.10.6 and restoring the default theme for now.

--- RandomizerHost/App.axaml.cs
+++ RandomizerHost/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using RandomizerHost.ViewModels;
 using RandomizerHost.Views;
+using Avalonia.Themes.Default;
 
 namespace RandomizerHost
 {
