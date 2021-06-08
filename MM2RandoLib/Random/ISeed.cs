109c159b5feb29bee7b21822ac8d1a54e2796505
Jason Dagit <dagitj@gmail.com>
2021-06-07 21:24:05 -0700

    Simplify RWeaknesses a bit with a new utility method

--- MM2RandoLib/Random/ISeed.cs
+++ MM2RandoLib/Random/ISeed.cs
@@ -50,6 +50,22 @@ namespace MM2Randomizer.Random
         T NextElement<T>(IEnumerable<T> in_Elements);
         IList<T> Shuffle<T>(IEnumerable<T> in_List);
 
+        /// <summary>
+        /// Similar to NextElement, but it deletes
+        /// the element from the dictionary.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="in_Dictionary"></param>
+        /// <returns></returns>
+        KeyValuePair<TKey, TValue> NextElementAndRemove<TKey, TValue>(IDictionary<TKey, TValue> in_Dictionary)
+        {
+            Int32 count = in_Dictionary.Count();
+            Int32 index = this.NextInt32(count);
+            KeyValuePair<TKey, TValue> element = in_Dictionary.ElementAt(index);
+            in_Dictionary.Remove(element);
+            return element;
+        }
+
         public Dictionary<TKey, TValue> Shuffle<TKey, TValue>(IDictionary<TKey, TValue> in_Dict)
         {
             List<TValue> values = Shuffle(in_Dict.Values).ToList();
