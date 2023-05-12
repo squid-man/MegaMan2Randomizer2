91a4de56d2a2b6cf24fabb7d547978dd9a51c757
Quantam <justin.olbrantz@gmail.com>
2023-05-11 18:12:03 -0700

    Working but not entirely finished phase 2 implementation for testing

---
+++ MM2RandoLib/Enums/ESoundTrackUsage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MM2Randomizer.Enums
+{
+    [Flags]
+    public enum ESoundTrackUsage
+    {
+        Intro = 1,
+        Title = 2,
+        StageSelect = 4,
+        Stage = 8,
+        Boss = 16,
+        Ending = 32,
+        Credits = 64,
+    }
+}
