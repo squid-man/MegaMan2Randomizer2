13ea9a0ab7cfd8321f48437204bdcc2f18294055
Justin Olbrantz <justin.olbrantz@gmail.com>
2026-05-02 00:37:08 -0700

    Split desktop/browser builds

---
+++ RandomizerHost.Browser/Program.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Browser;
+using ReactiveUI.Avalonia;
+using RandomizerHost;
+using System.Threading.Tasks;
+
+internal sealed partial class Program
+{
+    private static Task Main(string[] args) => BuildAvaloniaApp()
+            .StartBrowserAppAsync("out");
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+#if DEBUG
+            .WithDeveloperTools()
+#endif
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI(b => { });
+}
