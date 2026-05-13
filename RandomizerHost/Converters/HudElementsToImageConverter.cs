8c70e1a4f54b01f1e27c6ba0f98952fba9bf5e20
Justin Olbrantz <justin.olbrantz@gmail.com>
2026-05-13 01:24:22 -0700

    Fix sprite previews

--- RandomizerHost/Converters/HudElementsToImageConverter.cs
+++ RandomizerHost/Converters/HudElementsToImageConverter.cs
@@ -3,10 +3,12 @@ using Avalonia.Data;
 using Avalonia.Data.Converters;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using MM2Randomizer;
 using MM2Randomizer.Settings.Options;
 using System;
 using System.Diagnostics;
 using System.Globalization;
+using System.Reflection;
 
 namespace RandomizerHost.Converters;
 
@@ -20,7 +22,7 @@ public class HudElementsToImageConverter : IValueConverter
         if (value is not HudElementOption || !targetType.IsAssignableFrom(typeof(Bitmap)))
             throw new NotSupportedException();
 
-        Uri uri = new($"avares://{nameof(RandomizerHost)}/Assets/HudElementsSpritePreviews/HudElements_{value.ToString()}.png");
+        Uri uri = new($"avares://{Assembly.GetExecutingAssembly().GetShortName()}/Assets/HudElementsSpritePreviews/HudElements_{value.ToString()}.png");
         var asset = AssetLoader.Open(uri);
         return new Bitmap(asset);
     }
