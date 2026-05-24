4471a05d1baadc8814a720bf6473ec99fe82c685
Justin Olbrantz <justin.olbrantz@gmail.com>
2026-05-23 20:17:38 -0700

    Split AppConfigurationSettings.cs

---
+++ RandomizerHost/Settings/OptionGroupJsonConverter.cs
@@ -0,0 +1,49 @@
+﻿using MM2RandoLib.Settings.Options;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RandomizerHost.Settings;
+
+class OptionGroupJsonConverter : JsonConverter<OptionGroup>
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeToConvert.IsAssignableTo(typeof(OptionGroup));
+    }
+
+    public override OptionGroup? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, OptionGroup value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        foreach (var opt in value.Options)
+            JsonSerializer.Serialize(writer, opt, options);
+
+        foreach (var (mbr, mbrInfo) in value.MemberInfos)
+        {
+            object? mbrValue;
+            if (mbrInfo is PropertyInfo propInfo)
+                mbrValue = propInfo.GetValue(value);
+            else if (mbrInfo is FieldInfo fieldInfo)
+                mbrValue = fieldInfo.GetValue(value);
+            else
+                continue;
+
+            if (mbrValue is not OptionGroup grp)
+                continue;
+
+            writer.WritePropertyName(mbrInfo.Name);
+            JsonSerializer.Serialize(writer, grp, options);
+        }
+
+        writer.WriteEndObject();
+    }
+}
---
+++ RandomizerHost/Settings/RandomizationSettingsJsonConverter.cs
@@ -0,0 +1,64 @@
+﻿using MM2RandoLib.Settings.Options;
+using MM2Randomizer.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RandomizerHost.Settings;
+
+class RandomizationSettingsJsonConverter : JsonConverter<RandomizationSettings>
+{
+    record ClassStackEntry(Dictionary<string, ClassStackEntry> Objects, Dictionary<string, object> Values);
+
+    public override RandomizationSettings? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        RandomizationSettings settings = new();
+
+        var jsonDoc = JsonDocument.ParseValue(ref reader);
+
+        ReadGroup(settings, jsonDoc.RootElement, "", settings);
+
+        return settings;
+
+    }
+
+    public override void Write(Utf8JsonWriter writer, RandomizationSettings value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ReadGroup(
+        RandomizationSettings settings,
+        JsonElement grpEl,
+        string grpPath,
+        OptionGroup grp)
+    {
+        Type type = grp.GetType();
+        foreach (var en in grpEl.EnumerateObject())
+        {
+            string propName = en.Name;
+            string propPath = grpPath.Length != 0
+                ? $"{grpPath}.{propName}"
+                : propName;
+            if (settings.OptionsByPath.TryGetValue(propPath, out var iopt))
+            {
+                var optEl = en.Value;
+                var optType = iopt.Type;
+                if (optEl.TryGetProperty("Value", out var valueEl))
+                {
+                    if (optType == typeof(bool))
+                        iopt.BaseValue = valueEl.GetBoolean();
+                    else
+                        iopt.BaseValue = iopt.ParseType(valueEl.GetString()!);
+                }
+
+                if (optEl.TryGetProperty("Randomize", out var rndEl))
+                    iopt.Randomize = rndEl.GetBoolean();
+            }
+            else if (settings.GroupsByPath.TryGetValue(propPath, out var propGrp))
+                ReadGroup(settings, en.Value, propPath, propGrp);
+        }
+    }
+}
