5861fe8da77ad18023f7d12df1f1a4f5b8995ce7
TheRealQuantam <54358053+TheRealQuantam@users.noreply.github.com>
2026-06-06 13:15:55 -0700

    Update js65 version (#259)

--- MM2RandoLib/Extensions/AssemblerExtensions.cs
+++ MM2RandoLib/Extensions/AssemblerExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using js65;
 using System;
 using System.Diagnostics;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MM2Randomizer.Extensions
@@ -13,7 +14,21 @@ namespace MM2Randomizer.Extensions
             var res = task.Result;
             Debug.Assert(res is not null);
 
-            return res;
+            // TODO: Error handling for this should be improved at some point
+            if (!res.success)
+            {
+                StringBuilder sb = new();
+                sb.AppendLine("Assembly compilation failed");
+                foreach (var msg in res.messages)
+                {
+                    sb.AppendLine();
+                    sb.AppendLine(msg.ToString());
+                }
+
+                throw new Exception(sb.ToString());
+            }
+
+            return res.romdata;
         }
     }
 }
