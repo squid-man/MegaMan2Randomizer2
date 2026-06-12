c196b95973114cd056e5b7ff7f1007a4cd5211d1
TheRealQuantam <54358053+TheRealQuantam@users.noreply.github.com>
2026-06-12 14:57:31 -0700

    Copy Z2R's method of including Git info in version display (#261)
    * Copy Z2R's method of including Git info
    
    * Remove several unnecessary includes that were added for some reason

---
+++ RandomizerHost/GitInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomizerHost;
+
+public static partial class GitInfo
+{
+    public static bool IsOfficialBuild { get; } = false;
+    public static string Commit { get; } = "n/a";
+    public static string Branch { get; } = "unknown";
+    public static string Tag { get; } = "";
+    public static bool IsDirty { get; }
+}
