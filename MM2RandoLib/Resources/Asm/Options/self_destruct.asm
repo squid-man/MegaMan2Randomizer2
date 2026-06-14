7ebd9f8893daa5e79b4ba92e8b76cae8da0eeac3
Justin Olbrantz <justin.olbrantz@gmail.com>
2026-06-13 17:28:45 -0700

    Change self-destruct from up+A+start to up+A+select

--- MM2RandoLib/Resources/Asm/Options/self_destruct.asm
+++ MM2RandoLib/Resources/Asm/Options/self_destruct.asm
@@ -2,19 +2,34 @@
 
 .segment "BANKE"
 
-.org $817e ; Part of main game loop
-	jsr PatchDoPauseScreen ; Was jsr DoPauseScreen
+.org $8178 ; Part of main game loop
+	; 8178  A5 27          LDA $27
+	; 817A  29 08          AND #$08
+	; 817C  F0 03          BEQ $8181
+	; 817E  20 0D 83       JSR DoPauseScreen
 
-.reloc
-PatchDoPauseScreen:
-	; Okay to clobber A, X (Y??)
 	lda CtrlState
 	eor #$ff
-	and #$11 ; = 0 if Up and A are held
+	and #%00010101 ; Up, select, and A
+
+	jsr PatchCheckPause
+
+	FREE_UNTIL $8181
+
+.reloc
+PatchCheckPause:
+	; Okay to clobber A, X (Y??)
 	bne +
 
 	sta MmState ; Explosion only occurs if MmState == 0
 	jmp DoDeath
+
++
+	lda NewButtonsPressed
+	and #$8 ; Start
+	bne +
+
+	rts
 
 +
 	jmp DoPauseScreen
