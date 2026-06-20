d8a2d23de8f1e406755f0d26501a527290cf9a9f
warmCabin <ryanwb2@gmail.com>
2026-06-20 17:18:34 -0400

    update style in ice_stage.asm

--- MM2RandoLib/Resources/Asm/Options/ice_stage.asm
+++ MM2RandoLib/Resources/Asm/Options/ice_stage.asm
@@ -7,43 +7,43 @@
 
 .segment "BANKF"
 
-; Very end of bg_collision_check routine
+; Very end of BG collision check routine
 ; Was:
-;   LDA #$E
-;   JSR $C000
-;   RTS
-.org $CC41
-  JSR ice_stage_patch
-  JMP $C000 ; aka Switch16kBank
+;   lda #$e
+;   jsr $c000
+;   rts
+.org $cc41
+  jsr IceStagePatch
+  jmp $c000 ; aka Switch16kBank
 
-FREE_UNTIL $CC47
+FREE_UNTIL $cc47
 
 ; Overwrite Flash Man's special tile #3 to ground
-.org $CC52
+.org $cc52
   .byte $01
 
-FREE_UNTIL $CC53
+FREE_UNTIL $cc53
 
 .reloc
-ice_stage_patch:
-  LDA LevelIdx
-  CMP #ICE_STAGE
-  BNE @exit
+IceStagePatch:
+  lda LevelIdx
+  cmp #ICE_STAGE
+  bne @exit
   
   ; Enemies seem to do fine if the ground is ice, but this should help if you run into issues:
-  ; Only override for Mega Man. CurObjIdx isn't actually 00, but underflown to FF for him.
-  ; LDA CurObjIdx
-  ; BPL @exit
+  ; Only override for Mega Man. CurObjIdx isn't actually 00, but underflown to ff for him.
+  ; lda CurObjIdx
+  ; bpl @exit
 
   ; $00 contains the actual tile type (not just "special tile #2")
-  LDA $00
-  CMP #1
-  BNE @exit
+  lda $00
+  cmp #1
+  bne @exit
   
-  LDA #7
-  STA $00
+  lda #7
+  sta $00
 
 @exit:
-  ; We are responsible for switching back to bank E on the way out.
-  LDA #$E
-  RTS
+  ; We are responsible for switching back to bank e on the way out.
+  lda #$e
+  rts
