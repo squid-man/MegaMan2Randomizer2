ba309d0cea3a249dc5eb399a2fcac24f0dc87752
warmCabin <ryanwb2@gmail.com>
2026-06-26 01:45:30 -0400

    comply with code style

--- MM2RandoLib/Resources/Asm/Auto/sniper_armor_respawn_fix.asm
+++ MM2RandoLib/Resources/Asm/Auto/sniper_armor_respawn_fix.asm
@@ -11,48 +11,48 @@ LevelIdStash = $05 ; Seems to be safe ¯\_(ツ)_/¯
 .segment "BANKE"
 
 ; Physics call in state 0, where it's sitting still
-.org $B32D
-  JSR SniperArmorPatch1 ; Was JSR ObjPhysicsCollisionOnlyEnemy
+.org $b32d
+	jsr SniperArmorPatch1 ; Was jsr ObjPhysicsCollisionOnlyEnemy
 
-FREE_UNTIL $B330
+FREE_UNTIL $b330
 
 ; Physics call in states 1-6, where it's jumping or shooting
-.org $B3F2
-  JSR SniperArmorPatch1 ; Was JSR ObjPhysics
+.org $b3f2
+	jsr SniperArmorPatch1 ; Was jsr ObjPhysics
 
-FREE_UNTIL $B3F5
+FREE_UNTIL $b3f5
 
 ; Just spawned the Sniper Joe child object
-.org $B409
-  JMP SniperArmorPatch2 ; Was STA GenObjVars+$10,Y
-  ; RTS ; Since we're doing a tail call, this byte can be considered free.
+.org $b409
+	jmp SniperArmorPatch2 ; Was sta GenObjVars + $10, y
+	; rts ; Since we're doing a tail call, this byte can be considered free.
 
-FREE_UNTIL $B40D
+FREE_UNTIL $b40d
 
 .reloc
 SniperArmorPatch1:
-  ; X is OBJECT slot (10-1F)
-  ; ObjPhysics handles unloading the enemy, including clearing out the level ID. So we need to stash it.
+	; X is OBJECT slot (10-1f)
+	; ObjPhysics handles unloading the enemy, including clearing out the level ID. So we need to stash it.
 
-  ; Annoyingly, this address becomes $F0, which is zp. But this instruction MUST not assmble into a zp instruction.
-  ; Luckily it does assemble to a 2-byte address, but I don't know how to enforce that.
-  LDA EnemyLevelIds-$10,X
-  STA LevelIdStash
+	; Annoyingly, this address becomes $f0, which is zp. But this instruction MUST not assmble into a zp instruction.
+	; Luckily it does assemble to a 2-byte address, but I don't know how to enforce that.
+	lda EnemyLevelIds - $10, x
+	sta LevelIdStash
 
-  ; Both callsites go here, so check state and replace the correct sprite physics call.
-  LDA EnemyVars120-$10,X
-  BEQ +
-  JMP ObjPhysics ; Tail call!
+	; Both callsites go here, so check state and replace the correct sprite physics call.
+	lda EnemyVars120 - $10, x
+	beq +
+	jmp ObjPhysics ; Tail call!
 +:
-  JMP ObjPhysicsCollisionOnlyEnemy ; Tail call!
+	jmp ObjPhysicsCollisionOnlyEnemy ; Tail call!
 
 SniperArmorPatch2:
-  ; X is parent OBJECT slot (10-1F)
-  ; Y is child ENEMY slot (0-F)
-  ; Vanilla code has already checked the carry (indicating failed spawn), so we know this is valid.
-  STA GenObjVars+$10,Y
+	; X is parent OBJECT slot (10-1f)
+	; Y is child ENEMY slot (0-f)
+	; Vanilla code has already checked the carry (indicating failed spawn), so we know this is valid.
+	sta GenObjVars + $10, y
 
-  LDA LevelIdStash
-  STA EnemyLevelIds,Y
-  
-  RTS
+	lda LevelIdStash
+	sta EnemyLevelIds, y
+	
+	rts
