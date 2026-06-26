8ba34d89f634c3540c6e8910cc122f0851f9df60
Charlie Boy <MEACharlieboy@live.com>
2026-06-25 20:51:44 -0500

    Integrated feedback from Contributors

--- RandomizerHost.Common/Views/MainView.axaml.cs
+++ RandomizerHost.Common/Views/MainView.axaml.cs
@@ -361,5 +361,40 @@ namespace RandomizerHost.Views
                 // optionally log or show a user-visible error
             }
         }
+        private void WikiLink_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            var url = "https://github.com/squid-man/MegaMan2Randomizer2/wiki";
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
+            catch (Exception)
+            {
+                // optionally log or show a user-visible error
+            }
+        }
+        private void DiscordLink_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            var url = "https://discord.gg/CmRjkSe";
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
+            catch (Exception)
+            {
+            // optionally log or show a user-visible error
+            }
+        }
+
     }
 }
