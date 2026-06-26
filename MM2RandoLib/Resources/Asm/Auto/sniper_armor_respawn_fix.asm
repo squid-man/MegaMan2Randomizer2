e2e83e78649859013a44b579f4c7d8de90472636
warmCabin <ryanwb2@gmail.com>
2026-06-26 01:24:52 -0400

    update comments and variable names

--- MM2RandoLib/Resources/Asm/Auto/sniper_armor_respawn_fix.asm
+++ MM2RandoLib/Resources/Asm/Auto/sniper_armor_respawn_fix.asm
@@ -1,58 +1,58 @@
-; Snarpen your snife
+; Fixes a frustrating bug where a Sniper Armor can respawn even though its Sniper Joe operator is still alive.
+; The game already uses a table of level IDs at $100 to prevent respawns, so it's just a matter of passing the Armor's
+; level ID along to the Joe. As always, the devil's in the details.
 
 .include "mm2r.inc"
 
 ; Fixtures
 
-EnemyLevelIds = $100
-EnemyStates = $120 ; aka EnemyVars120
-SpriteTimers  = $4E0 ; aka GenObjVars
-sprite_physics = $EEBA
-sprite_physics_collision_only_enemy = $EFB3
-level_id_stash = $05 ; Seems to be safe :p
+LevelIdStash = $05 ; Seems to be safe ¯\_(ツ)_/¯
 
 .segment "BANKE"
 
-; Part where the snarmor snits sntill
+; Physics call in state 0, where it's sitting still
 .org $B32D
-  JSR SnarmorPatch ; Was JSR sprite_physics_collision_only_enemy
+  JSR SniperArmorPatch1 ; Was JSR ObjPhysicsCollisionOnlyEnemy
 
 FREE_UNTIL $B330
 
-; Part where the snarmor wants to snysics
+; Physics call in states 1-6, where it's jumping or shooting
 .org $B3F2
-  JSR SnarmorPatch ; Was JSR sprite_physics
+  JSR SniperArmorPatch1 ; Was JSR ObjPhysics
 
 FREE_UNTIL $B3F5
 
-; Part where the snarmor wants to spawn the snoe
+; Just spawned the Sniper Joe child object
 .org $B409
-  JMP SnarmorPatch2 ; Was STA SpriteTimers+$10,Y
+  JMP SniperArmorPatch2 ; Was STA GenObjVars+$10,Y
   ; RTS ; Since we're doing a tail call, this byte can be considered free.
 
 FREE_UNTIL $B40D
 
 .reloc
-SnarmorPatch:
-  ; X is SPRITE slot (10-1F)
-  ; sprite_physics handles unloading the enemy, including nulling out the level ID. So we need to stash it.
-  LDA EnemyLevelIds-$10,X ; Annoyingly, this becomes $F0, which is zp. But this instruction MUST not assmble into a zp instruction. Don't know how to enforce that.
-  STA level_id_stash
+SniperArmorPatch1:
+  ; X is OBJECT slot (10-1F)
+  ; ObjPhysics handles unloading the enemy, including clearing out the level ID. So we need to stash it.
+
+  ; Annoyingly, this address becomes $F0, which is zp. But this instruction MUST not assmble into a zp instruction.
+  ; Luckily it does assemble to a 2-byte address, but I don't know how to enforce that.
+  LDA EnemyLevelIds-$10,X
+  STA LevelIdStash
 
   ; Both callsites go here, so check state and replace the correct sprite physics call.
-  LDA EnemyStates-$10,X
+  LDA EnemyVars120-$10,X
   BEQ +
-  JMP sprite_physics ; Tail call!
+  JMP ObjPhysics ; Tail call!
 +:
-  JMP sprite_physics_collision_only_enemy ; Tail call!
+  JMP ObjPhysicsCollisionOnlyEnemy ; Tail call!
 
-SnarmorPatch2:
-  ; X is parent SPRITE slot (10-1F)
+SniperArmorPatch2:
+  ; X is parent OBJECT slot (10-1F)
   ; Y is child ENEMY slot (0-F)
-  ; Vanilla code has already checked the carry so we know this is good
-  STA SpriteTimers+$10,Y
+  ; Vanilla code has already checked the carry (indicating failed spawn), so we know this is valid.
+  STA GenObjVars+$10,Y
 
-  LDA level_id_stash
+  LDA LevelIdStash
   STA EnemyLevelIds,Y
   
   RTS
