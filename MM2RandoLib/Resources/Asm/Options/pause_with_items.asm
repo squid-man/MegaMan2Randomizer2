85ae1f2152cecefc519c6d206488560ed7b3c359
warmCabin <ryanwb2@gmail.com>
2026-06-27 16:51:46 -0400

    time stopper pause fix

--- MM2RandoLib/Resources/Asm/Options/pause_with_items.asm
+++ MM2RandoLib/Resources/Asm/Options/pause_with_items.asm
@@ -6,8 +6,20 @@
 
 .reloc
 DeleteBullets: 
-	; Delete objects 2-$f
-	ldx #$e
+	; Slot f only ever contains Flash Man's Time Stopper twinkle effect.
+	; That's the one object we actually want to prevent pausing, so check that slot separately.
+	lda ObjFlags + $f
+	bpl +
+
+	; The fabled double return!
+	; This basically tells the game to ignore your start press.
+	pla
+	pla
+	rts
+
++
+	; Delete objects 2-$e
+	ldx #$d
 
 -
 	lsr ObjFlags + 1, x
